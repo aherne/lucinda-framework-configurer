12fa204b74154c42d9118deee88efaf92c21c53f
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2019-10-04 06:48:47 +0300

    demo-ed throttling

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -5,5 +5,8 @@ class LoginController extends Lucinda\MVC\STDOUT\Controller
     {
         $this->response->attributes("csrf", $this->request->attributes("csrf")->generate(0));
         $this->response->attributes("status", $this->request->parameters("status"));
+        if ($waitTime = $this->request->parameters("wait")) {
+            $this->response->attributes("wait", $waitTime);
+        }
     }
 }
